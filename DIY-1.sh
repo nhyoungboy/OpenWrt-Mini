05a6004b50dc9ce9033636184eb9f638e59ea42b
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-12-24 15:31:21 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,15 +1,22 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
 
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-mosdns
+
 
 # 1-添加 MosDNS 插件
-git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+# git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 3-添加 opentomcat 主题
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 4-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
