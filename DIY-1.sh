b29d6521e6e0f118de2472e190ff5f08d2586496
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-12-24 21:30:12 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -4,7 +4,7 @@
 
 # 1-添加 MosDNS 插件
 # git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
@@ -14,6 +14,6 @@ git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 4-添加 OpenClash 插件
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 
