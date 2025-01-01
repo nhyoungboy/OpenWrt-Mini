70c7800a51a9ed83d2e78d3e0309741b220d5c4c
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-01 09:39:32 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,18 +1,21 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
+
+# 修复golang版本过低
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
 # 1-添加 PowerOff 插件
 git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
 
 # 2-添加 Mosdns 插件
-rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
-# git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
+git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
 
 # 3-添加 Openclash 插件
 wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
 unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
 rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
