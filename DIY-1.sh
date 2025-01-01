e6d32827f1752bd37fe890b367affe7a2a2ff7f1
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-01 15:02:01 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -14,10 +14,9 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
-make package/mosdns/luci-app-mosdns/compile V=s
+
 
 # 3-添加 Openclash 插件
 wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
