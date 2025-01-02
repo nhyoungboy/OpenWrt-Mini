25136be28301207222bad0091275748f4c436df8
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-02 08:14:45 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,31 +1,39 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
 
-# 修复golang版本过低
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# 1-添加 PowerOff 插件
-git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
-
-# 2-添加 Mosdns 插件
+sed -i '$a src-git gxnas https://github.com/gxnas/OpenWrt_Build_x64_Packages' feeds.conf.default
+# 移除要替换的包
+rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
-# sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
+rm -rf feeds/luci/applications/luci-app-docker
+rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf package/feeds/gxnas/natflow
+rm -rf package/feeds/gxnas/luci-app-dogcom
 
-# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
+
+
+
+ 
+# 1-添加 PowerOff 插件
+# git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
+
+# 2-添加 Mosdns 插件
+# rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
 # 3-添加 Openclash 插件
-wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
+# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+# unzip -d package/openclash package/openclash.zip
+# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+# rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
-sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+# sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
 
 # 5-添加  主题
 # rm -rf feeds/luci/themes/luci-theme-argon
